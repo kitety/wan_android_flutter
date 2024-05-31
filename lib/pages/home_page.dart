75f6263dc4d913e3f8b05bf2a17a9b393221e34d
kitety <aizaizuori@foxmail.com>
2024-05-31 20:21:55 +0800

    feat: 6 card style

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -46,18 +46,84 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _listItemView(int index) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            Image.network(
-              'https://picsum.photos/id/237/200/300',
-              width: 30.r,
-              height: 30.r,
-            )
-          ],
+    return Container(
+      margin: EdgeInsets.only(top: 5.h, left: 10.w, right: 10.w, bottom: 5.h),
+      padding: EdgeInsets.only(top: 8.h, left: 15.w, right: 15.w, bottom: 8.h),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black12, width: 0.5.r),
+        borderRadius: BorderRadius.all(
+          Radius.circular(6.r),
         ),
-      ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(40.r),
+                child: Image.network(
+                  'https://picsum.photos/id/237/200/300',
+                  width: 40.r,
+                  height: 40.r,
+                  fit: BoxFit.fill,
+                ),
+              ),
+              SizedBox(
+                width: 5.w,
+              ),
+              const Text(
+                '作者',
+                style: TextStyle(color: Colors.black),
+              ),
+              const Spacer(),
+              Text(
+                '2024-05-12 13:13',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 22.sp,
+                ),
+              ),
+              SizedBox(
+                width: 5.w,
+              ),
+              const Text(
+                '置顶',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          Text(
+            'Get a grayscale image by appending ?grayscale to the end of the url.',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 26.sp,
+            ),
+          ),
+          Row(
+            children: [
+              Text(
+                '分类',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22.sp,
+                ),
+              ),
+              const Spacer(),
+              Image.network(
+                'https://picsum.photos/id/237/200/300',
+                width: 30.r,
+                height: 30.r,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
